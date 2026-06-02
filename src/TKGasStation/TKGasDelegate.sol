0b1d7e793399ed852ae3630af860010c8186d27d
ngmihodl <157429619+ngmihodl@users.noreply.github.com>
2026-06-03 04:17:04 +0900

    execution failed error in assembly

--- src/TKGasStation/TKGasDelegate.sol
+++ src/TKGasStation/TKGasDelegate.sol
@@ -28,6 +28,7 @@ contract TKGasDelegate is EIP712, IERC1155Receiver, IERC721Receiver, IERC1271, I
     bytes4 internal constant DEADLINE_EXCEEDED_SELECTOR = 0x559895a3;
     bytes4 internal constant ERC1271_MAGIC_VALUE = 0x1626ba7e;
     bytes4 internal constant INVALID_OFFSET_SELECTOR = 0x01da1572;
+    bytes4 internal constant EXECUTION_FAILED_SELECTOR = 0xacfdb444;
 
     bytes32 internal constant EXECUTION_TYPEHASH = 0x06bb52ccb5d61c4f9c5baafc0affaba32c4d02864c91221ad411291324aeea2e;
     // keccak256("Execution(uint128 nonce,uint32 deadline,address to,uint256 value,bytes data)")
@@ -344,7 +345,7 @@ contract TKGasDelegate is EIP712, IERC1155Receiver, IERC721Receiver, IERC1271, I
         assembly {
             let ptr := mload(0x40)
             calldatacopy(ptr, _arguments.offset, _arguments.length)
-            if iszero(call(gas(), _outputContract, 0, ptr, _arguments.length, 0, 0)) { revert(0, 0) }
+            if iszero(call(gas(), _outputContract, 0, ptr, _arguments.length, 0, 0)) { mstore(0x00, EXECUTION_FAILED_SELECTOR) revert(0x00, 0x04) }
             // No need to restore free memory pointer - execution ends immediately
         }
     }
@@ -486,7 +487,7 @@ contract TKGasDelegate is EIP712, IERC1155Receiver, IERC721Receiver, IERC1271, I
         assembly {
             let ptr := mload(0x40)
             calldatacopy(ptr, _arguments.offset, _arguments.length)
-            if iszero(call(gas(), _outputContract, _ethAmount, ptr, _arguments.length, 0, 0)) { revert(0, 0) }
+            if iszero(call(gas(), _outputContract, _ethAmount, ptr, _arguments.length, 0, 0)) { mstore(0x00, EXECUTION_FAILED_SELECTOR) revert(0x00, 0x04) }
             // finish exection, no need to restore free memory pointer // mstore(0x40, add(ptr, _arguments.length))
         }
     }
@@ -612,7 +613,7 @@ contract TKGasDelegate is EIP712, IERC1155Receiver, IERC721Receiver, IERC1271, I
         assembly {
             let ptr := mload(0x40)
             calldatacopy(ptr, _arguments.offset, _arguments.length)
-            if iszero(call(gas(), _outputContract, _ethAmount, ptr, _arguments.length, 0, 0)) { revert(0, 0) }
+            if iszero(call(gas(), _outputContract, _ethAmount, ptr, _arguments.length, 0, 0)) { mstore(0x00, EXECUTION_FAILED_SELECTOR) revert(0x00, 0x04) }
             // No need to restore free memory pointer - execution ends immediately
         }
     }
@@ -738,7 +739,7 @@ contract TKGasDelegate is EIP712, IERC1155Receiver, IERC721Receiver, IERC1271, I
             assembly ("memory-safe") {
                 let ptr := mload(0x40)
                 calldatacopy(ptr, _callData.offset, _callData.length)
-                if iszero(call(gas(), outputContract, ethAmount, ptr, _callData.length, 0, 0)) { revert(0, 0) }
+                if iszero(call(gas(), outputContract, ethAmount, ptr, _callData.length, 0, 0)) { mstore(0x00, EXECUTION_FAILED_SELECTOR) revert(0x00, 0x04) }
                 mstore(0x40, add(ptr, _callData.length))
             }
         }
@@ -801,7 +802,7 @@ contract TKGasDelegate is EIP712, IERC1155Receiver, IERC721Receiver, IERC1271, I
             assembly ("memory-safe") {
                 let ptr := mload(0x40)
                 calldatacopy(ptr, _callData.offset, _callData.length)
-                if iszero(call(gas(), outputContract, ethAmount, ptr, _callData.length, 0, 0)) { revert(0, 0) }
+                if iszero(call(gas(), outputContract, ethAmount, ptr, _callData.length, 0, 0)) { mstore(0x00, EXECUTION_FAILED_SELECTOR) revert(0x00, 0x04) }
                 mstore(0x40, add(ptr, _callData.length))
             }
         }
@@ -890,7 +891,7 @@ contract TKGasDelegate is EIP712, IERC1155Receiver, IERC721Receiver, IERC1271, I
         assembly {
             let ptr := mload(0x40)
             calldatacopy(ptr, _arguments.offset, _arguments.length)
-            if iszero(call(gas(), _outputContract, _ethAmount, ptr, _arguments.length, 0, 0)) { revert(0, 0) }
+            if iszero(call(gas(), _outputContract, _ethAmount, ptr, _arguments.length, 0, 0)) { mstore(0x00, EXECUTION_FAILED_SELECTOR) revert(0x00, 0x04) }
             // No need to restore free memory pointer - execution ends immediately
         }
     }
@@ -1003,7 +1004,7 @@ contract TKGasDelegate is EIP712, IERC1155Receiver, IERC721Receiver, IERC1271, I
             assembly ("memory-safe") {
                 let ptr := mload(0x40)
                 calldatacopy(ptr, _callData.offset, _callData.length)
-                if iszero(call(gas(), outputContract, ethAmount, ptr, _callData.length, 0, 0)) { revert(0, 0) }
+                if iszero(call(gas(), outputContract, ethAmount, ptr, _callData.length, 0, 0)) { mstore(0x00, EXECUTION_FAILED_SELECTOR) revert(0x00, 0x04) }
                 mstore(0x40, add(ptr, _callData.length))
             }
         }
@@ -1496,7 +1497,7 @@ contract TKGasDelegate is EIP712, IERC1155Receiver, IERC721Receiver, IERC1271, I
             assembly ("memory-safe") {
                 let ptr := mload(0x40)
                 calldatacopy(ptr, _callData2.offset, _callData2.length)
-                if iszero(call(gas(), outputContract, ethAmount, ptr, _callData2.length, 0, 0)) { revert(0, 0) }
+                if iszero(call(gas(), outputContract, ethAmount, ptr, _callData2.length, 0, 0)) { mstore(0x00, EXECUTION_FAILED_SELECTOR) revert(0x00, 0x04) }
                 mstore(0x40, add(ptr, _callData2.length))
             }
         }
